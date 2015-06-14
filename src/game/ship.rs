495f886d85118ff7f110f3a84361c16ee94b3e01
meh <meh@schizofreni.co>
2015-06-14 19:46:25 +0200

    game/ship: add some accessors

--- src/game/ship.rs
+++ src/game/ship.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 use na::Vec3;
 
 pub enum Ship {
@@ -14,10 +16,44 @@ impl Details {
 			position: Vec3::new(0.0, 0.0, 0.0),
 		}
 	}
+
+	pub fn position(&self) -> &Vec3<f32> {
+		&self.position
+	}
+
+	pub fn update_position<F: Fn(Vec3<f32>) -> Vec3<f32>>(mut self, f: F) -> Self {
+		self.position = f(self.position);
+
+		self
+	}
+	
+	pub fn position_mut(&mut self) -> &mut Vec3<f32> {
+		&mut self.position
+	}
 }
 
 impl Ship {
 	pub fn cube() -> Self {
 		Ship::Cube(Details::new())
+	}
+}
+
+impl Deref for Ship {
+	type Target = Details;
+
+	fn deref(&self) -> &<Self as Deref>::Target {
+		match self {
+			&Ship::Cube(ref details) =>
+				details
+		}
+	}
+}
+
+impl DerefMut for Ship {
+	fn deref_mut(&mut self) -> &mut<Self as Deref>::Target {
+		match self {
+			&mut Ship::Cube(ref mut details) =>
+				details
+		}
 	}
 }
