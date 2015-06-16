cf6d5b4f76581ab05bfbcd6075e3430765423c17
meh <meh@schizofreni.co>
2015-06-16 22:12:29 +0200

    util: remove scale from utils

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,11 +1,5 @@
-use na::Mat4;
-
 pub mod color;
 pub use self::color::Color;
-
-pub fn scale(x: f32, y: f32, z: f32) -> Mat4<f32> {
-	Mat4::new(x, 0.0, 0.0, 0.0, 0.0, y, 0.0, 0.0, 0.0, 0.0, z, 0.0, 0.0, 0.0, 0.0, 1.0)
-}
 
 pub fn deg(v: f32) -> f32 {
 	::std::f32::consts::PI * v / 180.0
