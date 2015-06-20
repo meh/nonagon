6ce23e334b66e324400e563928fd1515108c6230
meh <meh@schizofreni.co>
2015-06-20 17:04:24 +0200

    renderer/video: cleanup whitespace

--- src/renderer/video.rs
+++ src/renderer/video.rs
@@ -33,7 +33,7 @@ impl<'a> Video<'a> {
 					varying vec2 v_texture;
 
 					void main() {
-						gl_Position  = vec4(position, 0.0, 1.0);
+						gl_Position = vec4(position, 0.0, 1.0);
 						v_texture = texture;
 					}
 				",
