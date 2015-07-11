99a7ae7c3c8953d053fda0cef393248b7be1b89a
meh <meh@schizofreni.co>
2015-07-11 13:20:20 +0200

    game/bullet: implement Update

--- src/game/bullet.rs
+++ src/game/bullet.rs
@@ -1,5 +1,5 @@
 use util::{Fill, Aspect};
-use super::{Position, Orientation, Velocity};
+use super::{Update, Position, Orientation, Velocity};
 
 #[derive(Debug)]
 pub enum Bullet {
@@ -29,6 +29,57 @@ impl Default for Bullet {
 			position:    Default::default(),
 			orientation: Default::default(),
 			velocity:    Default::default(),
+		}
+	}
+}
+
+impl Update for Bullet {
+	fn update(&mut self, aspect: &Aspect) {
+		#[inline(always)]
+		fn up(value: f32, velocity: f32, min: f32, max: f32, around: bool) -> f32 {
+			let new = value + velocity;
+
+			if new + velocity < min {
+				if around {
+					max
+				}
+				else {
+					min
+				}
+			}
+			else if new > max {
+				if around {
+					min
+				}
+				else {
+					max
+				}
+			}
+			else {
+				new
+			}
+		}
+
+		match self {
+			&mut Bullet::Sphere { ref mut position, ref mut orientation, ref velocity, .. } => {
+				position.x = up(position.x, velocity.x,    0.0, aspect.width() as f32,  false);
+				position.y = up(position.y, velocity.y,    0.0, aspect.height() as f32, false);
+				position.z = up(position.z, velocity.z, -100.0, 100.0,                  false);
+
+				orientation.roll  = up(orientation.roll,  velocity.roll,  0.0, 360.0, true);
+				orientation.pitch = up(orientation.pitch, velocity.pitch, 0.0, 360.0, true);
+				orientation.yaw   = up(orientation.yaw,   velocity.yaw,   0.0, 360.0, true);
+			},
+
+			&mut Bullet::Ray { ref mut position, ref mut orientation, ref velocity, .. } => {
+				position.x = up(position.x, velocity.x,    0.0, aspect.width() as f32,  false);
+				position.y = up(position.y, velocity.y,    0.0, aspect.height() as f32, false);
+				position.z = up(position.z, velocity.z, -100.0, 100.0,                  false);
+
+				orientation.roll  = up(orientation.roll,  velocity.roll,  0.0, 360.0, true);
+				orientation.pitch = up(orientation.pitch, velocity.pitch, 0.0, 360.0, true);
+				orientation.yaw   = up(orientation.yaw,   velocity.yaw,   0.0, 360.0, true);
+			}
 		}
 	}
 }
