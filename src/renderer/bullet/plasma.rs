09ec69a37c21cd0a4df0e93f264b82999600be59
meh <meh@schizofreni.co>
2015-07-14 14:21:40 +0200

    renderer/bullet/plasma: implement basic rendering

--- src/renderer/bullet/plasma.rs
+++ src/renderer/bullet/plasma.rs
@@ -1,22 +1,126 @@
-use glium::{Display, Surface};
+use glium::{Program, Display, VertexBuffer, Surface, DrawParameters};
+use glium::DepthTest::IfLessOrEqual;
+use glium::BlendingFunction::Addition;
+use glium::LinearBlendingFactor::{SourceAlpha, OneMinusSourceAlpha};
+use glium::index::NoIndices;
+use glium::index::PrimitiveType::TriangleStrip;
 
 use game;
 use renderer::{Render, Support};
+use util::Fill;
+
+#[derive(Copy, Clone)]
+pub struct Vertex {
+	position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
 
 pub struct Plasma<'a> {
 	display: &'a Display,
+
+	program:  Program,
+	vertices: VertexBuffer<Vertex>,
 }
 
 impl<'a> Plasma<'a>{
 	pub fn new<'b>(display: &'b Display) -> Plasma<'b> {
 		Plasma {
 			display: display,
+
+			program: program!(display,
+				110 => {
+					vertex: "
+						#version 110
+
+						attribute vec2 position;
+
+						uniform mat4 mvp;
+
+						varying vec2 v_position;
+
+						void main() {
+							gl_Position = mvp * vec4(position, 0.0, 1.0);
+							v_position  = position;
+						}
+					",
+
+					fragment: "
+						#version 110
+
+						uniform sampler2D bg;
+						uniform float width;
+						uniform float height;
+
+						uniform float border;
+
+						varying vec2 v_position;
+
+						void main() {
+							vec4 color = texture2D(bg, vec2(gl_FragCoord.x / width, gl_FragCoord.y / height));
+							color.r = 1.0 - color.r;
+							color.g = 1.0 - color.g;
+							color.b = 1.0 - color.b;
+
+							float dist = 1.0 - sqrt(v_position.x * v_position.x + v_position.y * v_position.y);
+							float t    = 0.0;
+
+							if (dist > border) {
+								t = 1.0;
+							}
+							else if (dist > 0.0) {
+								t = dist / border;
+							}
+
+							vec4 mixed = mix(vec4(color.rgb, 0.0), color, t);
+							mixed.r += 0.1;
+							mixed.g += 0.1;
+							mixed.b += 0.1;
+
+							gl_FragColor = mixed;
+						}
+					",
+				}
+			).unwrap(),
+
+			vertices: VertexBuffer::new(display, vec![
+				Vertex { position: [-1.0,  1.0] },
+				Vertex { position: [ 1.0,  1.0] },
+				Vertex { position: [-1.0, -1.0] },
+				Vertex { position: [ 1.0, -1.0] },
+			]),
 		}
 	}
 }
 
 impl<'a> Render<game::Bullet> for Plasma<'a> {
 	fn render<S: Surface>(&mut self, target: &mut S, support: &Support, state: &game::Bullet) {
-		// uguu~
+		if let &game::Bullet::Plasma { position, radius, .. } = state {
+			let mvp = support.scene().to_mat() *
+				support.scene().position(position) *
+				support.scene().scale(radius * 2.0) *
+				support.scene().depth(position);
+
+			let uniforms = uniform! {
+				mvp: mvp,
+
+				bg:     support.as_ref(),
+				width:  support.scene().width() as f32,
+				height: support.scene().height() as f32,
+
+				border: 0.5,
+			};
+
+			target.draw(&self.vertices, &NoIndices(TriangleStrip), &self.program, &uniforms, &DrawParameters {
+				blending_function: Some(Addition {
+					source:      SourceAlpha,
+					destination: OneMinusSourceAlpha
+				}),
+
+				depth_test:  IfLessOrEqual,
+				depth_write: true,
+
+				.. Default::default() }).unwrap();
+		}
 	}
 }
