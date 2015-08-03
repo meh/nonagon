e92feb9cc93a5ff7fd89a631cba2ea6defb96210
meh <meh@schizofreni.co>
2015-08-04 00:42:56 +0200

    util/ring: wrap a VecDeque

--- src/util/ring.rs
+++ src/util/ring.rs
@@ -1,62 +1,49 @@
-use std::ops::Deref;
+use std::collections::VecDeque;
+use std::ops::{Deref, DerefMut};
 
+#[derive(Debug)]
 pub struct Ring<T> {
-	buffer:   Vec<T>,
-	size:     usize,
-	position: usize,
+	buffer: VecDeque<T>,
+	size:   usize,
 }
 
 impl<T> Ring<T> {
 	pub fn new(size: usize) -> Self {
 		Ring {
-			buffer:   Vec::with_capacity(size),
-			size:     size,
-			position: 0,
+			buffer: VecDeque::with_capacity(size),
+			size:   size,
 		}
 	}
 
 	pub fn push(&mut self, value: T) {
 		if self.buffer.len() >= self.size {
-			self.buffer[self.position] = value;
-		}
-		else {
-			self.buffer.insert(self.position, value);
+			self.buffer.pop_front();
 		}
 
-		self.position += 1;
-
-		if self.position >= self.size {
-			self.position = 0;
-		}
+		self.buffer.push_back(value);
 	}
 
 	pub fn pop(&mut self) -> Option<T> {
-		if self.buffer.is_empty() {
-			return None;
-		}
-
-		let result = if self.buffer.len() < self.size {
-			self.pop()
-		}
-		else {
-			Some(self.buffer.remove(self.position))
-		};
-
-		if self.position != 0 {
-			self.position -= 1;
-		}
-		else {
-			self.position = self.buffer.len() - 1;
-		}
-
-		result
+		self.buffer.pop_front()
 	}
+
+	pub fn push_back() -> ! { unimplemented!(); }
+	pub fn pop_back() -> ! { unimplemented!(); }
+	pub fn push_front() -> ! { unimplemented!(); }
+	pub fn pop_front() -> ! { unimplemented!(); }
+	pub fn remove() -> ! { unimplemented!(); }
 }
 
 impl<T> Deref for Ring<T> {
-	type Target = Vec<T>;
+	type Target = VecDeque<T>;
 
 	fn deref(&self) -> &Self::Target {
 		&self.buffer
+	}
+}
+
+impl<T> DerefMut for Ring<T> {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.buffer
 	}
 }
