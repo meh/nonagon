d2bf68d0843599b29b35506427e497b4b4e8dfdb
meh <meh@schizofreni.co>
2015-08-03 02:16:24 +0200

    analyzer: first try at beat detection

--- src/analyzer/analyzer.rs
+++ src/analyzer/analyzer.rs
@@ -4,18 +4,18 @@ use std::ops::{Deref, DerefMut};
 
 use ffmpeg::frame;
 
-use super::{util, SpectralFlux};
+use super::{util, SpectralFlux, Threshold};
 
 #[derive(Debug)]
 pub enum Channel {
-	Left(Event),
-	Right(Event),
-	Mono(Event),
+	Left(f64, Event),
+	Right(f64, Event),
+	Mono(f64, Event),
 }
 
 #[derive(Debug)]
 pub enum Event {
-	None,
+	Beat(f64),
 }
 
 pub struct Analyzer {
@@ -29,26 +29,69 @@ impl Analyzer {
 		let (sample_sender, sample_receiver) = channel::<Vec<i16>>();
 
 		thread::spawn(move || {
-			let mut beat    = SpectralFlux::new(1024);
+			// The current sample number, needed to know where in the stream we are.
+			let mut index = 0;
+
+			// The spectral flux analyzer.
+			let mut beat = SpectralFlux::new(1024);
+
+			// The threshold for the fluxes (21 samples [about a second worth], and 1.5 sensitivity).
+			//
+			// This is needed so we can filter out noise from the actual beats.
+			let mut threshold = Threshold::new(21, 1.5);
+
+			// The buffer for the samples, so we can take out 1024 samples at a time.
 			let mut samples = Vec::new();
+
+			// The previous prunned flux, so we can detect beats.
+			let mut previous = 0.0;
 
 			loop {
 				samples.extend(ret!(sample_receiver.recv()));
 
-				// we need at least 1024 (2048 since it's stereo) samples before we can
-				// analyze the music
+				// We need at least 1024 (2048 since it's stereo) samples before we can
+				// analyze the music.
 				if samples.len() < 2048 {
 					continue;
 				}
 
-				// separate into channels and run FFT on them
+				// Separate into channels and run FFT on them.
 				let (mono, left, right) = util::channels(&samples[0..2048]);
 
+				// Get the current flux.
+				let current = beat.rising(&mono);
+
+				// Update the threshold with the new flux.
+				threshold.push(current);
+
+				// We have an outlier!
+				if current > threshold.current() {
+					if previous > current {
+						// The beat was actually in the previous sample.
+						let time = (index - 1024) as f64 / 44100.0;
+
+						// Normalize the flux with the threshold.
+						let flux = previous - threshold.current();
+
+						// Send the beat event.
+						event_sender.send(Channel::Mono(time, Event::Beat(flux))).unwrap();
+					}
+
+					// Set the previous so we can get a new beat.
+					previous = current - threshold.current();
+				}
+				else {
+					// Reset the previous to 0 so we can get a new beat.
+					previous = 0.0;
+				}
+
 				// TODO: find a more performant way, probably using VecDeques and doing
-				// partial merges or something
+				// partial merges or something.
 				for _ in 0 .. 2048 {
 					samples.remove(0);
 				}
+
+				index += 1024;
 			}
 		});
 
