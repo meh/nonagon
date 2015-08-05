7a61f9e9d0f731efbf8972cf9444354a33e4c827
meh <meh@schizofreni.co>
2015-08-05 18:31:55 +0200

    config: support passing more than one config file

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use std::fs::File;
 use std::io::Read;
 
@@ -47,7 +48,6 @@ pub use self::audio::Audio;
 pub mod video;
 pub use self::video::Video;
 
-
 #[derive(Clone, Default, Debug)]
 pub struct Config {
 	input: Option<String>,
@@ -61,32 +61,41 @@ pub struct Config {
 impl Config {
 	pub fn load(args: &ArgvMap) -> Result<Config, ParserError> {
 		let mut config = Config::default();
+		let     files  = args.get_vec("--config");
 
 		config.input = Some(String::from(args.get_str("<input>")));
 
-		if !args.get_str("--config").is_empty() {
-			if let Ok(mut file) = File::open(&args.get_str("--config")) {
-				let mut string = String::new();
-				file.read_to_string(&mut string).unwrap();
-
-				let mut parser = Parser::new(&string);
-
-				if let Some(toml) = parser.parse() {
-					try!(config.game.load(args, &Value::Table(toml.clone())));
-					try!(config.analyzer.load(args, &Value::Table(toml.clone())));
-					try!(config.audio.load(args, &Value::Table(toml.clone())));
-					try!(config.video.load(args, &Value::Table(toml.clone())));
-				}
-				else {
-					return Err(parser.errors.pop().unwrap());
-				}
-			}
-			else {
-				expect!("file not found");
+		if !files.is_empty() {
+			for file in &files {
+				try!(config.merge(args, &file));
 			}
 		}
 
 		Ok(config)
+	}
+
+	pub fn merge<P: AsRef<Path>>(&mut self, args: &ArgvMap, path: P) -> Result<(), ParserError> {
+		if let Ok(mut file) = File::open(path.as_ref()) {
+			let mut string = String::new();
+			file.read_to_string(&mut string).unwrap();
+
+			let mut parser = Parser::new(&string);
+
+			if let Some(toml) = parser.parse() {
+				try!(self.game.load(args, &Value::Table(toml.clone())));
+				try!(self.analyzer.load(args, &Value::Table(toml.clone())));
+				try!(self.audio.load(args, &Value::Table(toml.clone())));
+				try!(self.video.load(args, &Value::Table(toml.clone())));
+			}
+			else {
+				return Err(parser.errors.pop().unwrap());
+			}
+		}
+		else {
+			expect!("file not found");
+		}
+
+		Ok(())
 	}
 
 	pub fn input(&self) -> &str {
